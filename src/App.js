e7ad489dd426db6f3f9ea857f41e08a71d4bd51c
Galina <galina@Olegs-MacBook-Pro.local>
2019-12-03 22:58:21 +0300

    Created form with validation

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SimpleForm from "./SimpleForm";
+
+const App = () => (
+  <div>
+    <SimpleForm />
+  </div>
+);
+
+export default App;
---
+++ src/style.js
@@ -0,0 +1,23 @@
+const style = {
+  form: {
+    margin: 50,
+    padding: 10,
+    width: 300,
+    border: "5px solid pink",
+    backgroundColor: "black",
+    color: "white"
+  },
+  inputGroup: {
+    marginBottom: 10
+  },
+  input: {
+    backgroundColor: "#EFEFFF",
+    marginLeft: 10
+  },
+  error: {
+    color: "pink",
+    margin: 5
+  }
+};
+
+export default style;
